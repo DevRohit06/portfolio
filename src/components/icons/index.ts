085debde036990bc5c42fadfef70cd96a032e35f
Rohit Kushwaha <technicalrohit06@gmail.com>
2025-12-30 06:01:36 +0530

    feat: Add new UI icons and update index export

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -5,3 +5,18 @@ export { default as MailIcon } from "./Mail.astro";
 export { default as InstagramIcon } from "./Instagram.astro";
 export { default as DiscordIcon } from "./Discord.astro";
 export { default as Favicon } from "./Favicon.astro";
+
+// UI Icons
+export { default as CalendarIcon } from "./CalendarIcon.astro";
+export { default as RefreshIcon } from "./RefreshIcon.astro";
+export { default as StarIcon } from "./StarIcon.astro";
+export { default as GitForkIcon } from "./GitForkIcon.astro";
+export { default as EyeIcon } from "./EyeIcon.astro";
+export { default as AlertCircleIcon } from "./AlertCircleIcon.astro";
+export { default as ExternalLinkIcon } from "./ExternalLinkIcon.astro";
+export { default as LicenseIcon } from "./LicenseIcon.astro";
+export { default as GitBranchIcon } from "./GitBranchIcon.astro";
+export { default as CodeIcon } from "./CodeIcon.astro";
+export { default as FileTextIcon } from "./FileTextIcon.astro";
+export { default as NpmIcon } from "./NpmIcon.astro";
+export { default as GithubUI_Icon } from "./GithubUI_Icon.astro";
