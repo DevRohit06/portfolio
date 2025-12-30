f1fa5c62045f859860f072ee93430bc7ae108ee2
Rohit Kushwaha <technicalrohit06@gmail.com>
2025-12-30 06:01:40 +0530

    feat: Add resume and banner links to socials

--- src/lib/socials.ts
+++ src/lib/socials.ts
@@ -5,4 +5,6 @@ export const socials = {
   twitter: "https://twitter.com/rohitk_06",
   mail: "mailto:rohitk290106@gmail.com",
   discord: "https://discord.com/users/743173584935190620",
+  resume: "https://drive.google.com/file/d/198PUt1h0i3w3Nj-EtQ9L_RlsNdcz1xaz/view?usp=sharing",
+  banner: "https://raw.githubusercontent.com/DevRohit06/DevRohit06/refs/heads/main/banner.png",
 };
